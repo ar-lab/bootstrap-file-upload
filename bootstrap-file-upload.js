91d4c285af0e1e2faffb4018394d00115b1664b3
alex-ray <alex-ray@land.ru>
2017-02-28 16:43:21 +0300

    File Uploader for Bootstrap 2.3.2

---
+++ bootstrap-file-upload.js
@@ -0,0 +1,111 @@
+(function ($) {
+
+    $.fn.bootstrapFile = function (options) {
+
+        var settings = $.extend({
+            url: false,
+            btnText: 'Browse',
+            btnIcon: 'icon-folder-open',    // icon-folder-open | icon-file | icon-upload | icon-plus | icon-plus-sign
+            btnType: ''                     // btn-primary | btn-info | btn-success | btn-warning | btn-danger | btn-inverse
+        }, options);
+
+        var form = $('<form class="form-horizontal" method="post"></form>');
+
+        var bar = $('<div class="bar"></div>');
+        var percent = $('<div class="percent">0%</div>');
+        var progress = $('<div class="progress hidden"></div>').css('margin-top', '10px');
+        var status = $('<div id="status" class="hidden"></div>').css('margin-top', '10px');
+
+        progress.append(bar);
+        progress.append(percent);
+
+        function uploadFile() {
+
+            form.attr('method', "post");
+            form.attr('enctype', "multipart/form-data");
+
+            form.append(progress);
+            form.append(status);
+
+            form.ajaxForm({
+
+                url: settings.url,
+                // iframe: true,
+                // dataType: 'json',
+                // timeout: 3000,
+                // resetForm: true,
+                // clearForm: true,
+
+                beforeSend: function() {
+                    progress.removeClass('hidden');
+                    status.empty();
+                    status.addClass('hidden');
+                    var percentVal = '0%';
+                    bar.width(percentVal);
+                    percent.html(percentVal);
+                },
+                uploadProgress: function(event, position, total, percentComplete) {
+                    var percentVal = percentComplete + '%';
+                    bar.width(percentVal);
+                    percent.html(percentVal);
+                },
+                success: function() {
+                    var percentVal = '100%';
+                    bar.width(percentVal);
+                    percent.html(percentVal);
+                },
+                complete: function(xhr) {
+                    status.html(xhr.responseText);
+                    status.removeClass('hidden');
+                    form.removeAttr('method');
+                    form.removeAttr('enctype');
+                    progress.addClass('hidden');
+                }
+            });
+
+            form.submit();
+        }
+
+        return this.each(function (e) {
+
+            var self = $(this);
+            var clone = self.clone();
+
+            var fileInputName = clone.attr('name');
+
+            clone.css('display', 'none');
+
+            var icon = settings.btnIcon ? '<i class="' + settings.btnIcon + (settings.btnType ? ' icon-white' : '') + '"></i> ' : '';
+            var text = settings.btnText ? settings.btnText : '';
+            var type = settings.btnType ? ' ' + settings.btnType : '';
+
+            var group = $('<div class="input-append"></div>');
+
+            var input = $('<input class="input-large uneditable-input" name="' + fileInputName + '" value="">');
+            var button = $('<button class="btn' + type + '" type="button">' + icon + text + '</button>').click(function() {
+                clone.click();
+            });
+
+            clone.on('change', function() {
+                var uploadedFileName = $(this).val();
+                uploadedFileName = uploadedFileName.replace("C:\\fakepath\\", "");
+                input.val(uploadedFileName);
+                uploadFile();
+            });
+
+            // $(clone).appendTo(group);
+            // $(input).appendTo(group);
+            // $(button).appendTo(group);
+
+            $(group).append(input);
+            $(group).append(button);
+
+            $(group).appendTo(form);
+
+            self.replaceWith(form);
+
+        });
+
+    };
+
+}(jQuery));
